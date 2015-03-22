c3f95fc7aab7c8c9312ca4a5c0a511be9cbba0ab
Erik Sohns <eriksohns@123mail.org>
2015-03-22 04:03:38 +0000

    minor changes

--- src/include/olinuxino_config.h
+++ src/include/olinuxino_config.h
@@ -1,1 +1,231 @@
-../../build/debug/olinuxino_config.h
+#ifndef _OLINUXINO_CONFIG_H
+#define _OLINUXINO_CONFIG_H 1
+ 
+/* olinuxino_config.h. Generated automatically at end of configure. */
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if translation of program messages to the user's native
+   language is requested. */
+#ifndef OLINUXINO_ENABLE_NLS
+#define OLINUXINO_ENABLE_NLS 1
+#endif
+
+/* Define to 1 if you have the Mac OS X function CFLocaleCopyCurrent in the
+   CoreFoundation framework. */
+/* #undef HAVE_CFLOCALECOPYCURRENT */
+
+/* Define to 1 if you have the Mac OS X function CFPreferencesCopyAppValue in
+   the CoreFoundation framework. */
+/* #undef HAVE_CFPREFERENCESCOPYAPPVALUE */
+
+/* Define if the GNU dcgettext() function is already present or preinstalled.
+   */
+#ifndef OLINUXINO_HAVE_DCGETTEXT
+#define OLINUXINO_HAVE_DCGETTEXT 1
+#endif
+
+/* Define to 1 if you have the `getcwd' function. */
+#ifndef OLINUXINO_HAVE_GETCWD
+#define OLINUXINO_HAVE_GETCWD 1
+#endif
+
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#ifndef OLINUXINO_HAVE_GETTEXT
+#define OLINUXINO_HAVE_GETTEXT 1
+#endif
+
+/* Define if you have the iconv() function and it works. */
+/* #undef HAVE_ICONV */
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#ifndef OLINUXINO_HAVE_INTTYPES_H
+#define OLINUXINO_HAVE_INTTYPES_H 1
+#endif
+
+/* Define to 1 if you have the `ACE' library (-lACE). */
+#ifndef OLINUXINO_HAVE_LIBACE
+#define OLINUXINO_HAVE_LIBACE 1
+#endif
+
+/* Define to 1 if you have the `glut' library (-lglut). */
+#ifndef OLINUXINO_HAVE_LIBGLUT
+#define OLINUXINO_HAVE_LIBGLUT 1
+#endif
+
+/* Define to 1 if you have the <libintl.h> header file. */
+#ifndef OLINUXINO_HAVE_LIBINTL_H
+#define OLINUXINO_HAVE_LIBINTL_H 1
+#endif
+
+/* Define to 1 if you have the `pthread' library (-lpthread). */
+#ifndef OLINUXINO_HAVE_LIBPTHREAD
+#define OLINUXINO_HAVE_LIBPTHREAD 1
+#endif
+
+/* Define to 1 if you have the <locale.h> header file. */
+#ifndef OLINUXINO_HAVE_LOCALE_H
+#define OLINUXINO_HAVE_LOCALE_H 1
+#endif
+
+/* Define to 1 if your system has a GNU libc compatible `malloc' function, and
+   to 0 otherwise. */
+#ifndef OLINUXINO_HAVE_MALLOC
+#define OLINUXINO_HAVE_MALLOC 1
+#endif
+
+/* Define to 1 if you have the <memory.h> header file. */
+#ifndef OLINUXINO_HAVE_MEMORY_H
+#define OLINUXINO_HAVE_MEMORY_H 1
+#endif
+
+/* Define to 1 if you have the `memset' function. */
+#ifndef OLINUXINO_HAVE_MEMSET
+#define OLINUXINO_HAVE_MEMSET 1
+#endif
+
+/* Define to 1 if you have the `setlocale' function. */
+#ifndef OLINUXINO_HAVE_SETLOCALE
+#define OLINUXINO_HAVE_SETLOCALE 1
+#endif
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#ifndef OLINUXINO_HAVE_STDINT_H
+#define OLINUXINO_HAVE_STDINT_H 1
+#endif
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#ifndef OLINUXINO_HAVE_STDLIB_H
+#define OLINUXINO_HAVE_STDLIB_H 1
+#endif
+
+/* Define to 1 if you have the `strerror' function. */
+#ifndef OLINUXINO_HAVE_STRERROR
+#define OLINUXINO_HAVE_STRERROR 1
+#endif
+
+/* Define to 1 if you have the <strings.h> header file. */
+#ifndef OLINUXINO_HAVE_STRINGS_H
+#define OLINUXINO_HAVE_STRINGS_H 1
+#endif
+
+/* Define to 1 if you have the <string.h> header file. */
+#ifndef OLINUXINO_HAVE_STRING_H
+#define OLINUXINO_HAVE_STRING_H 1
+#endif
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#ifndef OLINUXINO_HAVE_SYS_STAT_H
+#define OLINUXINO_HAVE_SYS_STAT_H 1
+#endif
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#ifndef OLINUXINO_HAVE_SYS_TYPES_H
+#define OLINUXINO_HAVE_SYS_TYPES_H 1
+#endif
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#ifndef OLINUXINO_HAVE_UNISTD_H
+#define OLINUXINO_HAVE_UNISTD_H 1
+#endif
+
+/* Define if a version suffix is present. */
+/* #undef HAVE_VERSION_DEVEL */
+
+/* Define to 1 if the system has the type `_Bool'. */
+#ifndef OLINUXINO_HAVE__BOOL
+#define OLINUXINO_HAVE__BOOL 1
+#endif
+
+/* Name of package */
+#ifndef OLINUXINO_PACKAGE
+#define OLINUXINO_PACKAGE "OLinuxIno"
+#endif
+
+/* Define to the address where bug reports for this package should be sent. */
+#ifndef OLINUXINO_PACKAGE_BUGREPORT
+#define OLINUXINO_PACKAGE_BUGREPORT "eriksohns@123mail.org"
+#endif
+
+/* Define to the full name of this package. */
+#ifndef OLINUXINO_PACKAGE_NAME
+#define OLINUXINO_PACKAGE_NAME "olinuxino"
+#endif
+
+/* Define to the full name and version of this package. */
+#ifndef OLINUXINO_PACKAGE_STRING
+#define OLINUXINO_PACKAGE_STRING "olinuxino 0.0.1-devel"
+#endif
+
+/* Define to the one symbol short name of this package. */
+#ifndef OLINUXINO_PACKAGE_TARNAME
+#define OLINUXINO_PACKAGE_TARNAME "OLinuxIno"
+#endif
+
+/* Define to the home page for this package. */
+#ifndef OLINUXINO_PACKAGE_URL
+#define OLINUXINO_PACKAGE_URL "http://www.github.com/esohns/olinuxino"
+#endif
+
+/* Define to the version of this package. */
+#ifndef OLINUXINO_PACKAGE_VERSION
+#define OLINUXINO_PACKAGE_VERSION "0.0.1-devel"
+#endif
+
+/* Define to 1 if you have the ANSI C header files. */
+#ifndef OLINUXINO_STDC_HEADERS
+#define OLINUXINO_STDC_HEADERS 1
+#endif
+
+/* Version number of package */
+#ifndef OLINUXINO_VERSION
+#define OLINUXINO_VERSION "0.0.1-devel"
+#endif
+
+/* Define to 1 if `lex' declares `yytext' as a `char *' by default, not a
+   `char[]'. */
+/* #undef YYTEXT_POINTER */
+
+/* Define for Solaris 2.5.1 so the uint32_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT32_T */
+
+/* Define for Solaris 2.5.1 so the uint8_t typedef from <sys/synch.h>,
+   <pthread.h>, or <semaphore.h> is not used. If the typedef were allowed, the
+   #define below would cause a syntax error. */
+/* #undef _UINT8_T */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to the type of a signed integer type of width exactly 8 bits if such
+   a type exists and the standard includes do not define it. */
+/* #undef int8_t */
+
+/* Define to rpl_malloc if the replacement function should be used. */
+/* #undef malloc */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef ssize_t */
+
+/* Define to the type of an unsigned integer type of width exactly 16 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint16_t */
+
+/* Define to the type of an unsigned integer type of width exactly 32 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint32_t */
+
+/* Define to the type of an unsigned integer type of width exactly 8 bits if
+   such a type exists and the standard includes do not define it. */
+/* #undef uint8_t */
+ 
+/* once: _OLINUXINO_CONFIG_H */
+#endif
